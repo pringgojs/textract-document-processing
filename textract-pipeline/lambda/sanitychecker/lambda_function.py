432b0a3e8ec934553b4bbd8624436d221e19fd38
Hendry Anwar <hendryaw@amazon.com>
2021-04-11 17:56:14 +0700

    Update sanityChecker with generic implementation

--- textract-pipeline/lambda/sanitychecker/lambda_function.py
+++ textract-pipeline/lambda/sanitychecker/lambda_function.py
@@ -5,15 +5,14 @@ import re
 from decimal import Decimal
 from botocore.exceptions import ClientError
 
-def updateRecordVerificationStatus(id, doctype, passed):
+def updateRecordVerificationStatus(documentId, passed):
     dynamodb = boto3.resource('dynamodb')
     metadataTable = dynamodb.Table(os.environ["DOCUMENT_METADATA_TABLE"])
-    print("Updating the verification status on the record id: {} with result: {}".format(id, passed))
+    print("Updating the verification status on the record documentId: {} with result: {}".format(documentId, passed))
 
     response = metadataTable.update_item(
         Key={
-            'id': id,
-            'doctype': doctype
+            'documentId': documentId
         },
         UpdateExpression="set passedSanityCheck=:p",
         ExpressionAttributeValues={
@@ -30,53 +29,21 @@ def checkRecord(record):
         print("Record already have sanity check results with value: {}. Skipping the record.".format(newImage["passedSanityCheck"]["S"]))
         return
     
-    id = None
-    doctype = None
-    # Additional key-value pairs to be checked.
-    nib = None
-    nibConfidence = None
+    documentId = None
+    # Additional key-value pairs to be checked. For example name, address, etc.
+    # nama = None
     
-    if("id" in newImage and "S" in newImage["id"]):
-        id = newImage["id"]["S"]
+    if("documentId" in newImage and "S" in newImage["documentId"]):
+        documentId = newImage["documentId"]["S"]
 
-    if("doctype" in newImage and "S" in newImage["doctype"]):
-        doctype = newImage["doctype"]["S"]
-
-    if("kvsets" in newImage and "L" in newImage["kvsets"]):
-        for kvset in newImage["kvsets"]["L"]:
-            map = kvset["M"]
-            key = None
-            value = None
-            confidence = None
-
-            for k,v in map.items():
-                print ("k: {}, v: {}".format(k, v))
-                if (k == "key" and "S" in v):
-                    key = v["S"]
-                if (k == "value" and "S" in v):
-                    value = v["S"]
-                if (k == "confidence" and "N" in v):
-                    confidence = v["N"]
-                
-            if (key and key.lower() == "nib"):
-                nib = value
-                nibConfidence = confidence
-
-    print("id: {}, doctype: {}, nib: {}, nib confidence: {}".format(id, doctype, nib, nibConfidence))
-
+    # Process any additional validation here.
+    # For example Regex validation or confidence validation.
     passed = "false"
     print("Checking if NIB exists")
-    if (id and doctype and nib):
-        print("NIB exists.")
-        print("Checking if the NIB confidence level is above 20")
-        if (Decimal(nibConfidence) > 20):
-            print("NIB confidence is above 20.")
-            print("Checking if NIB follows the pattern of 8-digits number followed with dot (.) and followed with 5-digits number")
-            if (re.search(r"^[0-9]{8}\.{1}[0-9]{5}$", nib)):
-                print("The NIB follows the pattern")
-                passed = "true"
+    if (documentId):
+        passed = "true"
 
-    updateRecordVerificationStatus(id, doctype, passed)
+    updateRecordVerificationStatus(documentId, passed)
 
 
 def lambda_handler(event, context):
