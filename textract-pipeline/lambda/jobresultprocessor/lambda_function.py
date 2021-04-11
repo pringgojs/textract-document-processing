e1c1c84611a813858e04cfc4b08c5a75e2c492ba
Hendry Anwar <hendryaw@amazon.com>
2021-04-11 17:44:03 +0700

    Refactor the JobResultProc function

--- textract-pipeline/lambda/jobresultprocessor/lambda_function.py
+++ textract-pipeline/lambda/jobresultprocessor/lambda_function.py
@@ -48,13 +48,11 @@ def getJobResults(api, jobId):
 def processRequest(request):
 
     output = ""
-    all_text = ""
 
     print(request)
 
     jobId = request['jobId']
     jobTag = request['jobTag']
-    jobStatus = request['jobStatus']
     jobAPI = request['jobAPI']
     bucketName = request['bucketName']
     objectName = request['objectName']
@@ -65,97 +63,59 @@ def processRequest(request):
     print("Result pages recieved: {}".format(len(pages)))
 
     detectForms = False
-    detectTables = False
     if(jobAPI == "StartDocumentAnalysis"):
         detectForms = True
-        detectTables = True
 
     # Start - Store the key-value sets in DynamoDB Document Metadata table
     document = Document(pages)
     if (document.pages):
         print("Total Pages in Document: {}".format(len(document.pages)))
 
+        documentItem = {
+            'documentId': jobTag,
+            'documentName': objectName,
+        }
+
+        allText = ""
         kvsets = []
         for page in document.pages:
+            allText = allText + page.text
             if (detectForms):
                 for field in page.form.fields:
-                    print("field: {}".format(field))
                     kvset  = {}
                     if(field.key):
                         kvset["key"] = field.key.text
-                        kvset["position_top"] = Decimal(str(field.key.geometry.boundingBox.top))
-                        kvset["position_left"] = Decimal(str(field.key.geometry.boundingBox.left))
-                        kvset["confidence"] = Decimal(str(field.key.confidence))
+                        kvset["key_position_top"] = Decimal(str(field.key.geometry.boundingBox.top))
+                        kvset["key_position_left"] = Decimal(str(field.key.geometry.boundingBox.left))
+                        kvset["key_position_width"] = Decimal(str(field.key.geometry.boundingBox.width))
+                        kvset["key_position_height"] = Decimal(str(field.key.geometry.boundingBox.height))
+                        kvset["key_confidence"] = Decimal(str(field.key.confidence))
 
-                    if(field.value):
-                        kvset["value"] = field.value.text
-                    else:
-                        kvset["value"] = ""
-
+                        if(field.value):
+                            documentItem[field.key.text] = field.value.text
+                            kvset["value"] = field.value.text
+                            kvset["value_position_top"] = Decimal(str(field.value.geometry.boundingBox.top))
+                            kvset["value_position_left"] = Decimal(str(field.value.geometry.boundingBox.left))
+                            kvset["value_position_width"] = Decimal(str(field.value.geometry.boundingBox.width))
+                            kvset["value_position_height"] = Decimal(str(field.value.geometry.boundingBox.height))
+                            kvset["value_confidence"] = Decimal(str(field.value.confidence))
+                        else:
+                            documentItem[field.key.text] = ""
+                            kvset["value"] = ""
                     kvsets.append(kvset)
-            
-            all_text = all_text + " " + page.text
+        documentItem["allText"] = allText
+        documentItem['forms'] = kvsets
 
-        id = None
-        doctype = "UNKNOWN"
-        for kvset in kvsets:
-            print("kvset: {}".format(kvset))
-            if ("key" in kvset and (kvset["key"].lower() == "nib" or kvset["key"].lower()[-3:] == "nib")):
-                id = kvset["value"]
-        #       doctype = "BUKU TANAH"
-                break
-
-        # try once more to get ID from raw text
-        if (not id):
-            id =  all_text[all_text.find("NIB"):all_text.find("\n",all_text.find("NIB")+5)].replace(":","").replace(" ","").replace("NIB","").replace("\n","")
-        
-        if (not id):
-            id = str(uuid.uuid4())
-
-
-
-        #for kvset in kvsets:
-        #    if ("key" in kvset and (kvset["key"].lower().find("keadaan tanah") != -1 or kvset["key"].lower() .find("peta") != -1)):
-        #        #doctype = "SURAT UKUR"
-        #        break
-        
-        
-        luas =""
-        # get doc type from all text
-        if (all_text.lower().find("sebidang tanah terletak dalam") != -1 or all_text.lower() .find("keadaan tanah") != -1):
-            doctype = "SURAT UKUR"
-            l1 = re.findall("[0-9]* m2",all_text)
-            if l1:
-                luas = l1[0]
-            
-        if (all_text.lower().find("nama pemegang hak") != -1 or all_text.lower() .find("pendaftaran peralihan hak") != -1):
-            doctype = "BUKU TANAH"
-        
-        r1 = re.findall("[0-9][0-9][.][0-9][0-9][.][0-9][0-9][.][0-9][0-9][.][0-9][.][0-9][0-9][0-9][0-9][0-9]",all_text.replace("\n",""))
-        
-        nomor_seri = ""
-        if r1:
-            nomor_seri = r1[0]    
+        print("DEBUG - documentItem: {}".format(documentItem))
 
         dynamodb = boto3.resource('dynamodb')
         metadataTable = dynamodb.Table(os.environ["DOCUMENT_METADATA_TABLE"])
-        # insert all text as well for future use
-        print("Creating a new document in the Document Metadata table with id: {} , doctype: {} and kvsets: {}".format(id, doctype, kvsets))
-        response = metadataTable.put_item(
-            Item={
-                'id': id,
-                'doctype':doctype,
-                'kvsets': kvsets,
-                'text': all_text,
-                'nomor_seri': nomor_seri,
-                'luas': luas
-            }
-        )
+        print("Creating a new item in the Document Metadata table with content: {}".format(documentItem))
+        response = metadataTable.put_item(Item = documentItem)
         print("Response: {}".format(response))
     # End - store the key-value sets in DynamoDB Document Metadata table
 
     print("DocumentId: {}".format(jobTag))
-
     ds = datastore.DocumentStore(documentsTable)
     ds.markDocumentComplete(jobTag)
 
