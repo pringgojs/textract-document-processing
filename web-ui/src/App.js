2b46a33e1fe579f3d43a604efabd12de389c9899
Hendry Anwar <hendryaw@amazon.com>
2021-04-13 15:13:11 +0700

    Implement json viewer based on the DocumentMetadata

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { Auth } from 'aws-amplify';
+import { Auth, API } from 'aws-amplify';
 import AWS from 'aws-sdk';
-import { Button, Box, Container, Paper, Grid, Typography } from '@material-ui/core';
+
+import { Button, Box, Paper, Grid, Typography, AppBar, Toolbar } from '@material-ui/core';
+import RefreshIcon from '@material-ui/icons/Refresh';
 import { withStyles } from '@material-ui/core/styles';
+import ReactJson from 'react-json-view'
+
 import config from "./config.json";
 
 const styles = theme => ({
@@ -12,9 +16,14 @@ const styles = theme => ({
   },
   paper: {
     padding: theme.spacing(1),
-    textAlign: 'center',
     color: theme.palette.text.secondary,
-  }
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  title: {
+    flexGrow: 1,
+  },
 });
 
 const s3DocumentsBucketName = config.DOCUMENTS_UPLOAD_BUCKET.BUCKET_NAME;
@@ -26,20 +35,23 @@ class App extends Component {
     this.state = { 
       progress: 0,
       selectedFile: null,
-      uploadState: "Not Started"
+      uploadState: "Not Started",
+      selectedFileName: "No file chosen",
+      documentMetadataItems: []
     };
   }
   
   async componentDidMount() {
     try {
       let creds = await Auth.currentCredentials();
-      console.log(creds);
       AWS.config.update({
         accessKeyId: creds.accessKeyId,
         secretAccessKey: creds.secretAccessKey,
         sessionToken: creds.sessionToken,
         region: s3DocumentsBucketRegion
       });
+
+      await this.refreshDocumentMetadata();
     } catch (error) {
       console.log(error);
     }
@@ -47,6 +59,7 @@ class App extends Component {
 
   handleFileInput = (e) => {
     this.setState({ selectedFile: e.target.files[0] });
+    this.setState({ selectedFileName: e.target.files[0].name });
   };
 
   uploadFile = async (file) => {
@@ -73,34 +86,58 @@ class App extends Component {
     }
   }
 
+  getDocumentMetadata = () => { 
+    const apiName = 'DocumentMetadataService';
+    const path = '';
+    const init = {
+      headers: {},
+    };
+  
+    return API.get(apiName, path, init);
+  }
+
+  refreshDocumentMetadata = async () => { 
+    const response = await this.getDocumentMetadata();
+    this.setState({ documentMetadataItems: response });
+  }
+
   render() {
     const { classes } = this.props;
     return (
-      <React.Fragment>
-        <Box>
-          <AmplifySignOut />
-        </Box>
-        <Box my={4}>
-          <div className={classes.root}>
-            <Grid container spacing={1}>
-              <Grid item xs={12} sm={4}>
-                <Paper className={classes.paper}>
-                  <Typography variant="subtitle1">
-                    File Upload Progress is {this.state.progress}% - {this.state.uploadState}
-                  </Typography>
-                  <input type="file" onChange={this.handleFileInput}/>
-                  <Button variant="contained" onClick={() => this.uploadFile(this.state.selectedFile)}>Upload to S3</Button>
-                </Paper>
-              </Grid>
-              <Grid item xs={12} sm={8}>
-                <Paper className={classes.paper}>
-
-                </Paper>
-              </Grid>
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              AWS Textract Document Processing
+            </Typography>
+            <Button className={classes.button} variant="contained" color="default" onClick={() => this.refreshDocumentMetadata()}>
+              <RefreshIcon></RefreshIcon>Refresh
+            </Button>
+            <AmplifySignOut />
+          </Toolbar>
+        </AppBar>
+        <Box my={1}>
+          <Grid container spacing={1}>
+            <Grid item xs={12} sm={4}>
+              <Paper className={classes.paper}>
+                <Typography variant="subtitle1">
+                  File Upload Progress is {this.state.progress}% - {this.state.uploadState}
+                </Typography>
+                <Button className={classes.button} variant="contained" component="label">
+                  {this.state.selectedFileName}
+                  <input type="file" onChange={this.handleFileInput} hidden />
+                </Button>
+                <Button className={classes.button} variant="contained" color="primary" onClick={() => this.uploadFile(this.state.selectedFile)}>Upload to S3</Button>
+              </Paper>
             </Grid>
-          </div>
+            <Grid item xs={12} sm={8}>
+              <Paper className={classes.paper}>
+                <ReactJson src={this.state.documentMetadataItems} collapsed={2} enableClipboard={false} displayDataTypes={false} />
+              </Paper>
+            </Grid>
+          </Grid>
         </Box>
-      </React.Fragment>
+      </div>
     );
   }
 }
--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Amplify, Auth } from "aws-amplify";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from "aws-amplify";
 import config from "./config.json";
 
 Amplify.configure({
   Auth: {
-      // REQUIRED - Amazon Cognito Region
-      region: config.AMPLIFY.AUTH.REGION,
+    // REQUIRED - Amazon Cognito Region
+    region: config.AMPLIFY.AUTH.REGION,
 
-      // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
-      identityPoolId: config.AMPLIFY.AUTH.IDENTITY_POOL_ID,
+    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+    identityPoolId: config.AMPLIFY.AUTH.IDENTITY_POOL_ID,
 
-      // OPTIONAL - Amazon Cognito User Pool ID
-      userPoolId: config.AMPLIFY.AUTH.USER_POOL_ID,
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: config.AMPLIFY.AUTH.USER_POOL_ID,
 
-      // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-      userPoolWebClientId: config.AMPLIFY.AUTH.USER_POOL_WEB_CLIENT_ID
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: config.AMPLIFY.AUTH.USER_POOL_WEB_CLIENT_ID
+  },
+  API: {
+    endpoints: [
+      {
+          name: "DocumentMetadataService",
+          endpoint: config.AMPLIFY.API.DOCUMENT_METADATA_SERVICE_ENDPOINT,
+          custom_header: async () => { 
+            return { Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` }
+          }
+      }
+    ]
   }
 });
 
