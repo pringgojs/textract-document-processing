5fda9f334ee9ec53a17138f8540d95892e544423
Hendry Anwar <hendryaw@amazon.com>
2021-04-13 09:11:56 +0700

    Using existing backend resources from CDK

--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -5,6 +5,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from "aws-amplify";
 
+Amplify.configure({
+  Auth: {
+      // REQUIRED - Amazon Cognito Region
+      region: 'ap-southeast-1',
+
+      // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+      identityPoolId: 'ap-southeast-1:e6f73938-bf63-47f7-92f7-aba25631ce92',
+
+      // OPTIONAL - Amazon Cognito User Pool ID
+      userPoolId: 'ap-southeast-1_EaNaTlyw8',
+
+      // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+      userPoolWebClientId: '66eqvlvgcv5k2nb6q2uis54cop'
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
